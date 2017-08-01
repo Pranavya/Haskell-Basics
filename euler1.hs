7725676911524c5152d21befee73e80d65f8edd9
Pranavya <lomdom12345@gmail.com>
2017-08-01 23:16:43 +0530

    Updated the problem statement

--- euler1.hs
+++ euler1.hs
@@ -1,5 +1,8 @@
+--If we list all the natural numbers below 10 that are multiples of 3 or 5
+--sum of these multiples is 23.
+
+--Find the sum of all the multiples of 3 or 5 below 1000.
+
 euler001 n = sum [x | x <- [0 .. n], x `mod` 3 == 0  || x `mod` 5 == 0]
 
 main = putStrLn $ show (euler001 999)
-
-
